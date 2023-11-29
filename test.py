e1726ead149e2689328e050a91f2cac0fe0d2c6b
Alon <alon.horesh1@gmail.com>
2023-11-29 17:03:16 +0200

    VERSION_NUMBER = '00.03.00' VERSION_DESCRIPTION = 'templates website' VERSION_DATE = '29/11/2023'

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ from AOB_API import *
 if __name__ == '__main__':
     try:
         # use this email to signup, receive key , secret pair
-        status_code = signup(email='portfolio4planner@gmail.com')
         status_code = signup(email='home@network.com')
     except Exception as e:
         logging.exception(str(e))
